f5bcd51c85962eb59566b6ffe739a4162f72f259
alexatorr <torresa2@ufl.edu>
2024-07-24 18:49:08 -0400

    fixing somethings

--- at_Lab9.py
+++ at_Lab9.py
@@ -1,11 +1,15 @@
+#Alexa's code
 def encode(password):
-    intpass = list(map(int,password))
-    passencode=[]
+    intpass = list(password)
+    passencode=''
     i=0
     while i<=len(intpass)-1:
-        a=(intpass[i]+3)
-        passencode.append(a)
+        a=int(intpass[i])+3
+        if a>=10:
+            a=a-10
+        passencode=passencode+str(a)
         i=i+1
     strencode=''.join(str(passencode))
     print(strencode)
 encode('12345555')
+encode('00009962')
